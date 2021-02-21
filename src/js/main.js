d7c610b7757545af23fecd6b6d82f61cade3284a
Dusan Djordjic <dukidjordjic@gmail.com>
2021-02-21 13:37:13 +0100

    JS, img-scrs, footer
    finished javascript for home page, added images to website and mage footer

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+$('.nav-expanded').hide();
+$('.nav-arrow').click(function(event){
+    event.preventDefault();
+    $('.nav-expanded').slideToggle();
+});
+
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if ($(window).scrollTop() > 300){
+            $('.to-top').addClass("tt-show");
+        }
+        else{
+            $('.to-top').removeClass("tt-show");
+        }
+    });
+});
+    
+$(document).ready(function(){
+    var src;
+    $('.card-img img').hover(
+        function(){
+            $(this).fadeOut('fast', function () {
+                src = $(this).attr('src');
+                let newSrc = src.replace('-tshirt','');
+                $(this).attr('src',newSrc);
+                $(this).fadeIn('fast');
+            });
+        },
+        function(){
+            $(this).fadeOut('fast', function () {
+                let newSrc = src ;
+                $(this).attr('src',newSrc);
+                $(this).fadeIn('fast');
+            });
+        });
+
+})
